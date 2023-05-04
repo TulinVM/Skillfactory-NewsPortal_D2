080a4ac93fa21bfcd13da4999e9f11cb60a5aac1
TulinVM <124674817+TulinVM@users.noreply.github.com>
2023-05-04 15:38:32 +0300

    Delete NewsPortal_D2 directory

--- NewsPortal_D2/authors/__init__.py
+++
@@ -1,4 +0,0 @@
-TYPE_CHOICES = (
-    ('A', 'Article'),
-    ('N', 'News'),
-)
--- NewsPortal_D2/authors/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class PostForm(forms.Form):
-    title = forms.CharField()
-    text = forms.CharField(widget=forms.Textarea)
